5c33656cbab9cba19d8add3287153470594d06e1
Владислав Тоницой <tonvladrey@MacBook-Air-Vladislav.local>
2025-07-07 11:10:14 +0300

    first commit

---
+++ tests/test_new_password.py
@@ -0,0 +1,17 @@
+import string
+from password.new_password import generate_password
+
+def test_password_characters():
+    """Тест, что при генерации используются только допустимые символы"""
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    for char in password:
+        assert char in valid_characters
+
+"""
+Допиши еще один тест из предложенных. Или придумай свой.
+Если сможешь написать больше, то будет круто!
+
+Тест, что длина пароля соответствует заданной
+Тест, что два сгенерированных подряд пароля различаются
+"""
